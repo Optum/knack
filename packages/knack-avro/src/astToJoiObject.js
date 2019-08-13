35bdc9c6c20ef2ee4d47949af1b837c2785009d3
misterlantz <christopher.lantz@optum.com>
2019-08-13 10:47:26 -0500

    Start to add joi object creation

--- packages/knack-avro/src/astToJoiObject.js
+++ packages/knack-avro/src/astToJoiObject.js
@@ -47,6 +47,148 @@ const buildMappings = node => {
 	return mapField({...node, props});
 };
 
+const integerValidation = ({optional = true, description = null, minimum = null, maximum = null, greater = null, less = null}) => {
+	let s = 'Joi.number().integer()';
+
+	if (!optional) {
+		s += '.required()';
+	}
+
+	if (minimum !== null) {
+		s += `.min(${minimum})`;
+	}
+
+	if (maximum !== null) {
+		s += `.max(${maximum})`;
+	}
+
+	if (greater !== null) {
+		s += `.greater(${greater})`;
+	}
+
+	if (less !== null) {
+		s += `.less(${less})`;
+	}
+
+	if (description !== null) {
+		s += `.description("${description}")`;
+	}
+
+	return s;
+};
+
+const numberValidation = ({optional = true, description = null, minimum = null, maximum = null, greater = null, less = null}) => {
+	let s = 'Joi.number()';
+
+	if (!optional) {
+		s += '.required()';
+	}
+
+	if (minimum !== null) {
+		s += `.min(${minimum})`;
+	}
+
+	if (maximum !== null) {
+		s += `.max(${maximum})`;
+	}
+
+	if (greater !== null) {
+		s += `.greater(${greater})`;
+	}
+
+	if (less !== null) {
+		s += `.less(${less})`;
+	}
+
+	if (description !== null) {
+		s += `.description("${description}")`;
+	}
+
+	return s;
+};
+
+const booleanValidation = ({optional = true, description = null}) => {
+	let s = 'Joi.boolean()';
+
+	if (!optional) {
+		s += '.required()';
+	}
+
+	if (description !== null) {
+		s += `.description("${description}")`;
+	}
+
+	return s;
+};
+
+const dateValidation = ({optional = true, description = null}) => {
+	let s = 'Joi.date()';
+
+	if (!optional) {
+		s += '.required()';
+	}
+
+	if (description !== null) {
+		s += `.description("${description}")`;
+	}
+
+	return s;
+};
+
+const timeValidation = ({optional = true, description = null}) => {
+	let s = 'Joi.date().timestamp()';
+
+	if (!optional) {
+		s += '.required()';
+	}
+
+	if (description !== null) {
+		s += `.description("${description}")`;
+	}
+
+	return s;
+};
+
+const stringValidation = ({optional = true, description = null, minimum = null, maximum = null, valid = []}) => {
+	let s = 'Joi.string()';
+
+	if (!optional) {
+		s += '.required()';
+	}
+
+	if (minimum !== null) {
+		s += `.min(${minimum})`;
+	}
+
+	if (maximum !== null) {
+		s += `.max(${maximum})`;
+	}
+
+	if (Array.isArray(valid) && valid.length > 0) {
+		s += `.valid(${JSON.stringify(valid)})`;
+	}
+
+	if (description !== null) {
+		s += `.description("${description}")`;
+	}
+
+	return s;
+};
+
+const emailValidation = ({optional = true, description = null}) => {
+	let s = 'Joi.string().email()';
+
+	if (!optional) {
+		s += '.required()';
+	}
+
+	if (description !== null) {
+		s += `.description("${description}")`;
+	}
+
+	return s;
+};
+
 const main = ast => {
 	const mappings = buildMappings(ast.tree);
 	const {properties, type} = mappings[ast.tree.name];
