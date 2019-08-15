ba46100934e3b014b16be4a1e3014ed3ff39ac96
lxghtless <william.kurth@optum.com>
2019-08-15 15:34:46 -0500

    xo trailing space fix

--- packages/knack-cli/src/runConsumer.js
+++ packages/knack-cli/src/runConsumer.js
@@ -70,7 +70,7 @@ const main = async ({topic, consumerConfig: consumerConfigPath, topicConfig: top
 			subscriptions: [subscription],
 			flowMode: true,
 			consumerConfig,
-			topicConfig, 
+			topicConfig,
 			srOptions
 		});
 
